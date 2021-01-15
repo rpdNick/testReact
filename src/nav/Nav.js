1befa50ee435cf5f54ce3488df87dba92454bf53
rpdNick <rpd_nick@ukr.net>
2021-01-15 14:53:58 +0200

    react state

--- src/nav/Nav.js
+++ src/nav/Nav.js
@@ -2,10 +2,24 @@ import React from 'react';
 import './Nav.css';
 
 class Nav extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: "Navigation",
+            subtitle: "Main menu",
+            show: "hide"
+        }
+    }
+    ShowMenu = () => {
+        this.setState({show:"show"});
+    }
     render() {
      let nav = this.props.nav;
         return (
            <nav>
+               <button type="button" onClick={this.ShowMenu}>Show menu</button>
+               <h1>{this.state.title}</h1>
+               <p>{this.state.show}</p>
                <ul>
                     {Object.keys(nav).map(elem =>{
                         return <li key={elem}><a href="{nav[elem]}">{elem}</a></li>
